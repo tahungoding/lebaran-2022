8f78650b4a7c504bd06e78bc6c769db03d9633ad
fadliRafidan <rafidfid28@gmail.com>
2022-04-20 00:05:11 +0700

    responsive

--- src/components/Sarana.jsx
+++ src/components/Sarana.jsx
@@ -9,91 +9,81 @@ import terminal from '../../assets/img/terminal.png'
 
 export default function Sarana() {
     return (
-        <div className='md:mt-16 sm:mt-10 ss:mt-10 xm:mt-7 lg:mx-auto md:container' id='sarana'>
 
-
-
+        <div>
             <div className="container justify-center text-center lg:text-3xl md:text-2xl font-sans font-medium
-        text-[#3F3D56] ss:text-lg">
-                Sarana Prasarana Pendukung Perjalanan Mudik
-                <p className='lg:text-xl md:text-base sm:text-sm xs:text-[12px] xs:w-72 xs:leading-4  text-[#8B8B8B] font-sans leading-7 h-14 xm:w-[509px] mx-auto md:w-[590px]
-                 ss:text-[12px] ss:leading-4 '>  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Posuere viverra sit est egestas tristique.</p>
-
-            </div>
-
-            <div className='xs:flex justify-center 2xl:gap-32 xl:gap-16 lg:gap-5 md:gap-4 sm:gap-8 xs:gap-3 xm:inline-block xm:mx-auto xm:container xm:gap-y-8 ss:mx-auto ss:container ss:gap-y-8
-             ss:px-6
-            '>
-
-
-                <a href="/spbu">
-                    <Content
-                        content={{
-                            img: spbu,
-                            title: "SPBU"
-                        }}
-
-                    />
-                </a>
-                <a href="/bengkel">
-                    <Content
-                        content={{
-                            img: bengkel,
-                            title: "Bengkel"
-                        }}
-
-                    />
-                </a>
-
-                <a href="/minimarket">
-                    <Content
-                        content={{
-                            img: minimarket,
-                            title: "Mini Market"
-                        }}
-
-                    />
-                </a>
-
-                <a href="/atm">
-                    <Content
-                        content={{
-                            img: atm,
-                            title: "ATM"
-                        }}
-
-                    />
-                </a>
-            </div>
-
-            <div className="xs:flex justify-center 2xl:gap-32 xl:gap-16 lg:gap-5 md:gap-4 sm:gap-8 xs:gap-3 ss:inline-block ss:mx-auto ss:container ss:gap-y-8 ss:translate-x-4 ss:px-6 xs:translate-x-2 xs:px-6">
-                <div className="">
-
-                    <a href="/masjid">
-                        <Content
-                            content={{
-                                img: masjid,
-                                title: "Masjid"
-                            }}
-                        />
-                    </a>
+            text-[#3F3D56] ss:text-lg">
+                    Sarana Prasarana
+                    <p className='lg:text-xl md:text-base sm:text-sm xs:text-[12px] xs:w-72 xs:leading-4  text-[#8B8B8B] font-sans leading-7 h-14 md:w-[509px] mx-auto
+                     ss:text-[12px] ss:leading-4'>  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Posuere viverra sit est egestas tristique.</p>
                 </div>
-                <div className="">
 
 
-                    <a href="/terminal">
-                        <Content
-                            content={{
-                                img: terminal,
-                                title: "Terminal"
-                            }}
-                        />
-
-                    </a>
-                </div>
-            </div>
+                    <div className="min-h-full flex justify-center items-center container">
+            
+                        <div className="ss:flex-1 sm:flex-none min-w-2xl mx-auto ">
 
 
+                            <div className='grid ss:grid-cols-1 sm:grid-cols-2 md:grid-cols-4 md:gap-x-3 xl:grid-cols-4 xl:gap-x-76
+                       lg:grid-cols-4 lg:gap-x-5 
+                            '>
+                                <a href="/spbu">
+                                    <Content
+                                        content={{
+                                            img: spbu,
+                                            title: "SPBU"
+                                        }}
+                                    />
+                                </a>
+                                <a href="/bengkel">
+                                    <Content
+                                        content={{
+                                            img: bengkel,
+                                            title: "Bengkel"
+                                        }}
+                                    />
+                                </a>
+                                <a href="/minimarket">
+                                    <Content
+                                        content={{
+                                            img: minimarket,
+                                            title: "Mini Market"
+                                        }}
+                                    />
+                                </a>
+                                <a href="/atm">
+                                    <Content
+                                        content={{
+                                            img: atm,
+                                            title: "ATM"
+                                        }}
+                                    />
+                                </a>
+            
+                                    <a href="/masjid">
+                                        <Content
+                                            content={{
+                                                img: masjid,
+                                                title: "Masjid"
+                                            }}
+                                        />
+                                    </a>
+            
+                                    <a href="/terminal">
+                                        <Content
+                                            content={{
+                                                img: terminal,
+                                                title: "Terminal"
+                                            }}
+                                        />
+                                    </a>
+                                </div>
+                        </div>
+                    </div>
         </div>
+
+
+
+                
     )
 }
